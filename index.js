909af2107bbbcc4b4fd5cfc480804c968762d95a
Usama Ali <Usama@MacBook-Pro.local>
2020-10-25 15:12:45 +0500

    add environment variables

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
+require("dotenv").config();
 
-const url_string =
-  "mongodb+srv://admin:admin@cluster0.y9p4g.mongodb.net/quiz_app?retryWrites=true&w=majority";
+console.log(process.env);
+// const url_string =
+//   "mongodb+srv://admin:admin@cluster0.y9p4g.mongodb.net/quiz_app?retryWrites=true&w=majority";
 
-mongoose.connect(url_string, { useNewUrlParser: true }).then(() => {
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }).then(() => {
   console.log("server connected");
   const app = express();
   app.listen(process.env.PORT || 5000);
